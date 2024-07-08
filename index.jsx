dc3df21c2e6e2625fc44c3bea7bac3ddadc99d4d
Emanuel <joaem.lizarraga@gmail.com>
2024-07-08 19:50:05 -0300

    CreacionDeCursoGithub
    fix bug homescreen component - loggin

---
+++ index.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen - Not Implemented Yet</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;

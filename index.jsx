098db8c8a3cf6bb15d473f01e5a9c55a314e727c
Emanuel <joaem.lizarraga@gmail.com>
2024-07-09 19:35:11 -0300

    cambio de funcion por clase

--- index.jsx
+++ index.jsx
@@ -1,15 +1,14 @@
 /** @format */
 
-import React from "react";
 import { Text, View } from "react-native";
+import React, { Component } from "react";
 
-const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen - Not Implemented Yet</Text>
-      <Text> esta es otra implementacion</Text>
-    </View>
-  );
-};
-
-export default HomeScreen;
+export default class index extends Component {
+  render() {
+    return (
+      <View>
+        <Text>index</Text>
+      </View>
+    );
+  }
+}
